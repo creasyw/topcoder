d3723654639261797bf3bc65ec1443af7f96abfb
Qiong Wu <qiong@eero.com>
2018-10-31 10:08:53 -0700

    Add comments and test cases
    Signed-off-by: Qiong Wu <qiong@eero.com>

--- srm-1xx/2001-04/football.py
+++ srm-1xx/2001-04/football.py
@@ -31,6 +31,20 @@
 # value). If we simply adding the values up, then 2+3+2 and 2+2+3 would be regarded as two solutions.
 # To remove this kind of duplicated solutions, in each value, we have to also remember how many 2s,
 # 3s, and 7s in each solution.
+#
+# As a result, the next question is that "how to distinguish the same combinations with different
+# orders"? There are two ways. If we go with the previous thought to /only/ accumulate the sum,
+# there must be a way to store each unique solution for the given sum so that one can distinguish
+# the duplicated ways of a combination. An easier way is to map the three possbile scores as a 3D
+# plane where each point stands for a unique combination of three vectors. So, we only need to find
+# the decompositions of all of the vectors within a /given range/, we know how many unique points
+# are there for a specific value. That is the function =fetchCombinations_detailed=.
+#
+# Then, the solution get simpler... there is no need to store any of the value in that 3D cube.
+# Because we move the X, Y, Z axis as the numbers of 2-, 3-, and 7- points. It guarantees there is
+# no duplicated ways for a combination. Besides, none of the point uses previous values. So, we only
+# need to count the points that equals to the given value.
+
 
 
 class Football:
@@ -81,3 +95,12 @@ class Football:
                 register[index + i] += register[index]
 
         return register[-1]
+
+
+f = Football()
+assert f.fetchCombinations(2) == 1
+assert f.fetchCombinations(4) == 1
+assert f.fetchCombinations(34) == 19
+assert f.fetchCombinations(47) == 33
+assert f.fetchCombinations(1) == 0
+print("Everthing is passed")
