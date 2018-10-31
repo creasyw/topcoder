9d0e17dc8fc1551ffb91751d3204b69dbf162c86
Qiong Wu <qiong@eero.com>
2018-10-31 09:47:53 -0700

    Add solutions to the problem
    Signed-off-by: Qiong Wu <qiong@eero.com>

--- srm-1xx/2001-04/football.py
+++ srm-1xx/2001-04/football.py
@@ -24,3 +24,60 @@
 # Parameters:	int
 # Returns:	int
 # Method signature:	int fetchCombinations(int param0)
+#
+# It looks like a typical DP problem. It sounds like a DP problem. And, it is solved like a DP
+# problem. However, there is one bite hidding in the solution. The number of COMBINATIONS. It
+# conflicts with the typical DP steps deriving the value from left side (0) to the right side (input
+# value). If we simply adding the values up, then 2+3+2 and 2+2+3 would be regarded as two solutions.
+# To remove this kind of duplicated solutions, in each value, we have to also remember how many 2s,
+# 3s, and 7s in each solution.
+
+
+class Football:
+    def fetchCombinations(self, val):
+        counter = 0
+        for i in range(val // 7 + 1):
+            for j in range(val // 3 + 1):
+                for k in range(val // 2 + 1):
+                    if 2 * k + 3 * j + 7 * i == val:
+                        counter += 1
+        return counter
+
+    def fetchCombinations_detailed(self, val):
+        register = []
+        for i in range(val // 7 + 1):
+            plane = []
+            for j in range(val // 3 + 1):
+                row = []
+                for k in range(val // 2 + 1):
+                    row.append(2 * k + 3 * j + 7 * i)
+                plane.append(row)
+            register.append(plane)
+
+        counter = 0
+        for i in register:
+            for j in i:
+                for k in j:
+                    if k == val:
+                        counter += 1
+        return counter
+
+    def dup_fetchCombinations(self, score):
+
+        register = [0] * (score + 1)
+        possible_scores = [2, 3, 7]
+
+        for index in range(1, score + 1):
+
+            if index in possible_scores:
+                register[index] += 1
+
+            if register[index] == 0:
+                continue
+
+            for i in possible_scores:
+                if index + i >= score + 1:
+                    continue
+                register[index + i] += register[index]
+
+        return register[-1]
