cc7518661a5d39c0330c999e6363534a99677ed7
Qiong Wu <creasywuqiong@gmail.com>
2018-12-09 19:46:32 -0800

    First attempt
    Signed-off-by: Qiong Wu <qiongwu@utexas.edu>

--- tco/2013/get_min.py
+++ tco/2013/get_min.py
@@ -28,17 +28,87 @@
 # solution for the consequent problem for a larger scope -- all paires beyond the capacity are
 # subject to change.
 #
-# As a result, it is not a typicla fucking left-to-right sequential solved problem - because the
-# sub-problem are not from left to right (or right to left) based on the given lists. The sequences
-# do not matter. The essential part is to /find the sub problems/, whose combination is the final
-# solution.
+# It is not a typicla fucking incremental (aka. left-to-right sequential) problem - it is not
+# guaranteed that the solution of N pairs is larger than N-1 pairs. The sequence does not matter.
+# The residual capacity of a pair or pairs also does not really matter. The essential part is to
+# /find the sub problems/ that converts the pairs beyond capacity to within capacity. Finally,
+# combining all groups within capacity derives the final result.
 #
 # 【TCO13 Round 3B 450】AntlerSwapping 首先如果一个集合的鹿经过若干次交换之后可以让所有鹿都平衡，那
 # 么一定存在交换次数小于集合大小的方案（相当于找一颗最大的生成森林）。于是就可以状压DP，每一个状态只
 # 可能由自己内部交换或者由两个子集合并而来，内部交换的次数可以直接视为集合大小-1，因为更优的值一定可
 # 以从子集中合并上来。判断一个集合能不能都平衡只要对角的大小排序即可。
 
-class AntlerSwapping:
 
-    def getmin(a, b, c):
-        pass
+class Group:
+    def __init__(self, left, right, capacity):
+        self.register = sorted([left, right])
+        self.capacity = capacity
+        self.balanced = abs(left - right) <= capacity
+        if self.balanced:
+            self.counter = 0
+        else:
+            self.counter = -1
+
+    def __repr__(self):
+        return "Register: " + str(self.register) + ". Balanced:" + str(self.balanced) + ". Counter:" + str(self.counter)
+
+    @property
+    def max(self):
+        return self.register[-1]
+
+    @property
+    def min(self):
+        return self.register[0]
+
+    def merge(self, g2):
+        self.register = sorted(self.register + g2.register)
+        if self.balanced and g2.balanced:
+            self.counter += g2.counter
+            return
+        for i in range(0, len(self.register), 2):
+            if self.register[i + 1] - self.register[i] > self.capacity:
+                self.counter = -1
+                self.balanced = False
+                return
+        self.counter = len(self.register) // 2 - 1
+        self.balanced = True
+        return
+
+
+class AntlerSwapping:
+    def getmin(self, a, b, c):
+        register = []
+        for i in range(len(a)):
+            register.append(Group(a[i], b[i], c))
+
+        if len(register) == 1:
+            return register.balanced
+
+        register.sort(key=lambda k: k.min)
+        while len(register) > 1:
+            first = register[0]
+            second = register[1]
+            if first.max > second.min:
+                first.merge(second)
+                register.pop(1)
+                print(register)
+                continue
+            break
+
+        counter = 0
+        for group in register:
+            if group.counter == -1:
+                return -1
+            counter += group.counter
+
+        return counter
+
+
+sol = AntlerSwapping()
+
+#ans = sol.getmin([3,2,2], [3,5,5], 0)
+#print(ans, ans == 1)
+
+ans = sol.getmin([4, 2, 6, 4, 8, 5, 2, 3], [3, 4, 5, 2, 8, 5, 7, 6], 1)
+print(ans, ans == 2)
